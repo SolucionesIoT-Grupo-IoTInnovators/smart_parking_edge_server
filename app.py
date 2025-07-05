432161b49dff3a90cec4b0f1995f71adeb0eccd9
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2025-07-05 18:50:01 -0500

    feat: enhance MQTT integration with cloud and device clients
    Co-Authored-By: CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
    Co-Authored-By: FernandoDiazNeo <205968129+FernandoDiazNeo@users.noreply.github.com>
    Co-Authored-By: Johan Príncipe Godoy <73038573+JP19-03@users.noreply.github.com>
    Co-Authored-By: U202114545 <102629180+U202114545@users.noreply.github.com>
    Co-Authored-By: Ernesto Zarate <112828936+ern23zc@users.noreply.github.com>
    Co-Authored-By: anonimat0 <103579781+anonimat0@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import json
+
 from flask import Flask
 from dotenv import load_dotenv
 import os
+from getmac import get_mac_address
 
 from parking_spot.interfaces.services import parking_spot_api
 from iam.interfaces.services import iam_api
 from device.interfaces.services import device_api
 from shared.infrastructure.backend_connector import BackendApiClient
 from shared.infrastructure.database import init_db
-from shared.infrastructure.mqtt_client import mqtt_client, on_device_status_update, on_device_provisioning_request
+from shared.infrastructure.mqtt_client import mqtt_client_device, mqtt_client_cloud, on_device_status_update, \
+    on_device_provisioning_request, on_cloud_provisioning_response
 
 load_dotenv()
 
@@ -38,24 +42,39 @@ def initialize_mqtt():
         return
 
     print("Conectando a MQTT...")
-    if mqtt_client.connect(timeout=15):
+    if mqtt_client_cloud.connect(timeout=15):
+        print("✓ Conectado a MQTT broker para el backend")
+        mac_address = get_mac_address()
+        # Subscribe to the backend topics
+        provisioning_topic = os.getenv("MQTT_CLOUD_TOPIC_PROVISIONING_RESPONSE")
+
+        if provisioning_topic:
+            provisioning_subscribe = mqtt_client_cloud.subscribe(f"{provisioning_topic}{mac_address}", callback=on_cloud_provisioning_response)
+            print(f"✓ Suscrito a provisioning topic '{provisioning_topic}': {provisioning_subscribe}")
+
+        provision_request = { "mac": mac_address.lower() }
+        mqtt_client_cloud.publish(os.getenv("MQTT_CLOUD_TOPIC_PROVISIONING_REQUEST"), json.dumps(provision_request), qos=1)
+
+    else:
+        print("✗ Error conectando a MQTT broker para el backend")
+
+    if mqtt_client_device.connect(timeout=15):
         print("✓ Conectado a MQTT broker")
 
-        estado_topic = os.getenv("MQTT_TOPIC_ESTADO")
-        provisioning_topic = os.getenv("MQTT_TOPIC_PROVISIONING_REQUEST")
+        estado_topic = os.getenv("MQTT_DEVICE_TOPIC_ESTADO")
+        provisioning_topic = os.getenv("MQTT_DEVICE_TOPIC_PROVISIONING_REQUEST")
 
         if estado_topic:
-            estado_subscribe = mqtt_client.subscribe(estado_topic, callback=on_device_status_update)
+            estado_subscribe = mqtt_client_device.subscribe(estado_topic, callback=on_device_status_update)
             print(f"✓ Suscrito a reserva topic '{estado_topic}': {estado_subscribe}")
 
         if provisioning_topic:
-            provisioning_subscribe = mqtt_client.subscribe(provisioning_topic, callback=on_device_provisioning_request)
+            provisioning_subscribe = mqtt_client_device.subscribe(provisioning_topic, callback=on_device_provisioning_request)
             print(f"✓ Suscrito a provisioning topic '{provisioning_topic}': {provisioning_subscribe}")
 
         mqtt_initialized = True
     else:
         print("✗ Error conectando a MQTT broker")
-
 
 @app.before_request
 def setup():
@@ -69,4 +88,5 @@ if __name__ == '__main__':
         app.run(debug=True, use_reloader=False)
     finally:
         print("Desconectando MQTT...")
-        mqtt_client.disconnect()
+        mqtt_client_device.disconnect()
+        mqtt_client_cloud.disconnect()
