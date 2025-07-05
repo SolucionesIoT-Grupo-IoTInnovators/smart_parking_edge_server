0849b6cb9fa17b8f86b36ed56af05d9e3656c638
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2025-07-05 18:50:41 -0500

    feat: update EdgeServer and ParkingSpot repositories to use MAC address and improve server retrieval logic
    Co-Authored-By: CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
    Co-Authored-By: FernandoDiazNeo <205968129+FernandoDiazNeo@users.noreply.github.com>
    Co-Authored-By: Johan Príncipe Godoy <73038573+JP19-03@users.noreply.github.com>
    Co-Authored-By: U202114545 <102629180+U202114545@users.noreply.github.com>
    Co-Authored-By: Ernesto Zarate <112828936+ern23zc@users.noreply.github.com>
    Co-Authored-By: anonimat0 <103579781+anonimat0@users.noreply.github.com>

--- parking_spot/infrastructure/repositories.py
+++ parking_spot/infrastructure/repositories.py
@@ -5,17 +5,29 @@ from datetime import datetime
 class ParkingSpotRepository:
     @staticmethod
     def save(parking_spot):
-        spot = ParkingSpotModel.create(
-            spot_id = parking_spot.spot_id,
-            spot_label = parking_spot.spot_label,
-            status = parking_spot.status,
-            mac_address = parking_spot.mac_address,
-            parking_id = parking_spot.parking_id,
-            edge_id = parking_spot.edge_id,
-            device_type = parking_spot.device_type,
-            last_updated = datetime.now(),
-            created_at = datetime.now()
-        )
+        spot = ParkingSpotModel.get_or_none(spot_id=parking_spot.spot_id)
+        if not spot:
+            spot = ParkingSpotModel.create(
+                spot_id = parking_spot.spot_id,
+                spot_label = parking_spot.spot_label,
+                status = parking_spot.status,
+                mac_address = parking_spot.mac_address,
+                parking_id = parking_spot.parking_id,
+                edge_id = parking_spot.edge_id,
+                device_type = parking_spot.device_type,
+                last_updated = datetime.now(),
+                created_at = datetime.now()
+            )
+        else:
+            spot.spot_label = parking_spot.spot_label
+            spot.status = parking_spot.status
+            spot.mac_address = parking_spot.mac_address
+            spot.parking_id = parking_spot.parking_id
+            spot.edge_id = parking_spot.edge_id
+            spot.device_type = parking_spot.device_type
+            spot.last_updated = datetime.now()
+            spot.save()
+
         return ParkingSpot(
             spot_id=spot.spot_id,
             spot_label=spot.spot_label,
